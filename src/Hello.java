eb14e06b6ba4c436ba746eccaa6e98ce62114f43
mycolwang <mycol@126.com>
2019-02-16 13:42:00 +0800

    打印结果

---
+++ src/Hello.java
@@ -0,0 +1,11 @@
+/**
+ * @Description:
+ * @author: mycol
+ * @create: 2019/2/16
+ * @since: 1.0.0
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("This is my first GitDemo!");
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,9 @@
+/**
+ * @Description:
+ * @author: mycol
+ * @create: 2019/2/16
+ * @since: 1.0.0
+ */
+public class Test {
+
+}
